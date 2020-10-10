478aa1fe2d382f2a021af41e5bd827138bf3f5d6
Luis Lopes <luis.lopes@blip.pt>
2020-10-10 11:44:39 +0100

    Finished navigation init

--- gatsby/src/components/Nav.js
+++ gatsby/src/components/Nav.js
@@ -1,32 +1,27 @@
 import React from 'react';
-import { Link, navigate } from 'gatsby';
-
-// function goToSlicemasters() {
-//   // 1 - wait for 2 seconds
-//   setTimeout(() => {
-//     console.log('slicemasters');
-//     navigate('/slicemasters', { replace: true });
-//   }, 2000);
-
-//   // 2 - change page
-// }
+import { Link } from 'gatsby';
 
 export default function Nav() {
   return (
     <nav>
       <ul>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/">Hot Now</Link>
         </li>
         <li>
-          <Link href="/beers">Beers</Link>
+          <Link href="/pizzas">Pizza Menu</Link>
         </li>
-        {/* example of button navigation
         <li>
-          <button onClick={goToSlicemasters} type="button">
-            Click me to see slicemasters after 2 seconds
-          </button>
-        </li> */}
+          <Link href="/">LOGO</Link>
+        </li>
+
+        <li>
+          <Link href="/slicemasters">SliceMasters</Link>
+        </li>
+
+        <li>
+          <Link href="/order">Order Ahead!</Link>
+        </li>
       </ul>
     </nav>
   );
