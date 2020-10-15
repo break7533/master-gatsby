49b1d42a48b7ea4e54b3cdb89ec078999c950b9a
Luis Lopes <luis.lopes@blip.pt>
2020-10-15 23:29:53 +0100

    Gatsby API and sanity Graphql connection

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
     siteURL: `https:gatsby.pizza`,
     description: `The best pizza place in Porto`,
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // name of the plugin being added
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'wnc2io7k',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
