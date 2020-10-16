c6633bebff0fbf2193a1d3ce4278bcfc66b87bb2
Luis Lopes <luis.lopes@blip.pt>
2020-10-16 10:21:19 +0100

    Static queries and toppings filter

---
+++ gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,95 @@
+import { graphql, Link, useStaticQuery } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
+
+const ToppingStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+  }
+`;
+
+function countPizzasInToppings(pizzas) {
+  // Return the pizzas with counts
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      // Check if it an existing topping
+      const existingTopping = acc[topping.id];
+      // If it is, increment it
+      if (existingTopping) {
+        existingTopping.count += 1;
+      }
+      // otherwise create a new entry
+      else {
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+
+  // Sort based on their count
+  return Object.values(counts).sort((a, b) => b.count - a.count);
+}
+
+export default function ToppingsFilter() {
+  // Get a list of all the toppings
+  // Get a list of all the Pizzas with their toppings
+  const { toppings, pizzas } = useStaticQuery(graphql`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+          vegetarian
+        }
+      }
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            name
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  // Count how many pizzas are in each topping
+  const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+  console.clear();
+  console.log(toppingsWithCounts);
+
+  // Link it up
+  return (
+    <ToppingStyles>
+      {/* Loop over the list of toppings and display the topping and count of pizzas */}
+      {toppingsWithCounts.map((topping) => (
+        <Link key={topping.id} to={`/topping/${topping.name}`}>
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingStyles>
+  );
+}
