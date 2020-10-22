ce83d6ce9ad24c7aa9e44333a791ed10fb0d43f5
Luis Lopes <luis.lopes@blip.pt>
2020-10-23 00:04:22 +0100

    Skeleton screen and loading animation

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import LoadingGrid from '../components/LoadingGrid';
 import SEO from '../components/SEO';
+import { HomePageGrid } from '../styles/Grids';
 import useLatestData from '../utils/useLatestData';
 
-function CurrentlySlicing() {
+function CurrentlySlicing({ slicemasters }) {
   return (
     <div>
-      <p>Currently Slicing</p>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
     </div>
   );
 }
 
-function HotSlices() {
+function HotSlices({ hotSlices }) {
   return (
     <div>
-      <p>Hot Slices</p>
+      {!hotSlices && <LoadingGrid count={4} />}
+      {hotSlices && !hotSlices?.length && <p>Nothing in the case!</p>}
     </div>
   );
 }
@@ -26,10 +32,10 @@ export default function HomePage() {
       <div className="center">
         <h1>The Best Pizza Downtown!</h1>
         <p>Open 11am to 11pm Every Single Day</p>
-        <div>
+        <HomePageGrid>
           <CurrentlySlicing slicemasters={slicemasters} />
           <HotSlices hotSlices={hotSlices} />
-        </div>
+        </HomePageGrid>
       </div>
     </>
   );
